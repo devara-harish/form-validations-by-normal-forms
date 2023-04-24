bdc5e9916e6e144492cc4e2332872795d7c9d017
devara-harish <devaraharish09@gmail.com>
2023-04-24 19:05:46 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from app1.forms import *
+from django.http import HttpResponse
+# Create your views here.
+def Student(request):
+    SFO=StudentForm()
+    d={'SFO':SFO}
+    if request.method=='POST':
+        SFD=StudentForm(request.POST)
+        if SFD.is_valid():
+            return HttpResponse(str(SFD.cleaned_data))
+        else:
+            return HttpResponse('data is not valid')
+    return render(request,'Student.html',d)
